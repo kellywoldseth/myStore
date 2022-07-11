8688c4c803e96bb0b691d62795c8ce652722b73d
Kelly Woldsethgit config --global user.name Kelly <kelly.woldseth@gmail.com>
2022-07-11 13:19:21 -0700

    fixed errors

--- myStore/src/app/components/cart/cart.component.ts
+++ myStore/src/app/components/cart/cart.component.ts
@@ -1,13 +1,13 @@
 import { Component, Input, OnInit } from '@angular/core';
 import { Router } from '@angular/router';
-import {ProductService} from '../../services/product.service'
-import {CartService} from '../../services/cart.service'
-import {Product} from '../../models/product'
+import { ProductService } from '../../services/product.service';
+import { CartService } from '../../services/cart.service';
+import { Product } from '../../models/product';
 
 @Component({
   selector: 'app-cart',
   templateUrl: './cart.component.html',
-  styleUrls: ['./cart.component.css']
+  styleUrls: ['./cart.component.css'],
 })
 export class CartComponent implements OnInit {
   @Input() username: string = '';
@@ -17,26 +17,33 @@ export class CartComponent implements OnInit {
 
   cart: Product[] = [];
 
-  constructor(private router: Router, private productService: ProductService, private cartService: CartService) { }
+  constructor(
+    private router: Router,
+    private productService: ProductService,
+    private cartService: CartService
+  ) {}
 
   ngOnInit(): void {
     this.cart = this.cartService.getCart();
     this.total = this.cartService.calculateTotal();
   }
 
-  onSubmit():void
-  {
+  onSubmit(): void {
     this.router.navigate(['/confirmation']);
   }
 
-  clearCart(): void
-  {
+  clearCart(): void {
     this.cartService.clearCart();
-    this.cart=[];
+    this.cart = [];
+  }
+  
+  removeItem(product: Product): void
+  {
+    this.cartService.removeItemFromCart(product);
   }
 
-  
-
-
-
+  calculateTotal(): void
+  {
+    this.total = this.cartService.calculateTotal();
+  }
 }
